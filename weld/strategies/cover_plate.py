0fd46b6738d9279305c72de4ecaf71c4dd03ad63
Qi Deng <qideng@example.com>
2026-05-27 20:13:07 +0800

    refactor: move weld utilities out of region proposal

--- weld/strategies/cover_plate.py
+++
@@ -1,9 +0,0 @@
-"""CoverPlateStrategy: racetrack shape, works with generic + force_close."""
-
-from __future__ import annotations
-
-from weld.strategies.base import GenericStrategy
-
-
-class CoverPlateStrategy(GenericStrategy):
-    force_close: bool = True
